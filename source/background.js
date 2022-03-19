a66c43e3b40a68dc235ed4ca2e5aa71d2948583f
Sophie Snow <sophie.snoww@pm.me>
2022-03-19 00:09:54 -0600

    Everything

--- source/background.js
+++ source/background.js
@@ -1,2 +1,28 @@
 // eslint-disable-next-line import/no-unassigned-import
-import './options-storage.js';
+// import './options-storage.js';
+
+const Browser = require("webextension-polyfill");
+
+let index = null;
+
+Browser.tabs.onUpdated.addListener((id, info, tab) => {
+    Browser.tabs.query({index: tab.index}).then((tabs) => {
+        let tab = tabs[0];
+        
+        if (tab.url.match(/youtube\.com\/shorts\/([^?&=/]+)/)) {
+            if (index == null) {
+                index = tab.index;
+                let match = tab.url.match(/youtube\.com\/shorts\/([^?&=/]+)/);
+
+                Browser.tabs.update(tab.id, {
+                    url: `https://www.youtube.com/watch/${match[1]}`
+                });
+
+                setTimeout(() => {
+                    index = null;
+                }, 500);
+            }
+        }
+    });
+});
+
